7b4f84a3abedea6e5fd053cb6296c780e71f704f
Josh Green <josh@softwareverde.com>
2017-06-02 14:59:33 -0400

    Attempting to geocode.

--- parking-tickets/generate-sql.py
+++ parking-tickets/generate-sql.py
@@ -3,6 +3,27 @@ import sys
 import re
 import csv
 import MySQLdb
+import requests
+
+def find_closest_meter_id(lat, lng):
+    global connection
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT id, latitude, longitude, (POWER(latitude - %s, 2) + POWER(longitude - %s, 2)) AS distance FROM parking_meters WHERE latitude IS NOT NULL AND longitude IS NOT NULL HAVING distance < 0.001 ORDER BY distance ASC", (lat, lng))
+    row = cursor.fetchone()
+    if (row is not None):
+        row_id = row[0]
+        latitude = row[1]
+        longitude = row[2]
+        distance = row[3]
+        cursor.close()
+
+        # print("{}: ({}, {}) - ({}, {}) -> {}".format(row_id, lat, lng, latitude, longitude, distance))
+        return row_id
+    else:
+        cursor.close()
+        return None
+
 
 def levenshtein(s1, s2):
     if len(s1) < len(s2):
@@ -31,6 +52,11 @@ def is_number(s):
     except ValueError:
         return False
 
+def geocode_address(location):
+    r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address="+ location)
+    json = r.json()
+    return (json['results'][0]['geometry']['location']['lat'], json['results'][0]['geometry']['location']['lng'])
+
 connection = MySQLdb.connect(user='root', passwd='absolution', host='127.0.0.1', db='smart_city_api')
 
 def create_query(row):
@@ -40,36 +66,44 @@ def create_query(row):
     ticket_number = row[1].strip()
     plate_number = row[2].strip()
     plate_state = row[3].strip()
-    location = row[4].strip()
+    original_location = row[4].strip()
     violation_code = row[5].strip()
     total_fine = row[6].strip()
     fine_paid = row[7].strip()
     amount_due = row[8].strip()
     disposition = row[9].strip()
 
+    location = original_location
     match = re.search('^([0-9]+ )(.*)', location, re.IGNORECASE)
     if (match != None):
         first_word = match.group(1)
         location = match.group(2).strip()
 
     cursor = connection.cursor()
-    cursor.execute("SELECT id, location FROM parking_meters")
+    cursor.execute("SELECT location FROM parking_meters GROUP BY location")
     row = cursor.fetchone()
-    best_match = None
     best_match_location = None
     best_match_score = sys.maxint
     while (row != None):
-        row_id = row[0]
-        row_location = row[1]
+        row_location = row[0]
         score = levenshtein(row_location, location)
         if (score < best_match_score):
             best_match_score = score
-            best_match = row_id
             best_match_location = row_location
         row = cursor.fetchone()
 
-    print("{} matched to {} (score: {})".format(location, best_match_location, best_match_score))
+    # if (best_match_score > 5):
+    #     print("Failed to match: {}".format(location))
+    # print("{} matched to {} (score: {})".format(location, best_match_location, best_match_score))
+    if (best_match_score < 6):
+        latlng = geocode_address(original_location)
+        best_match = find_closest_meter_id(latlng[0], latlng[1])
+    else:
+        # print("Not A Meter: {}".format(original_location))
+        return
 
+
+    best_match = None
     if (best_match is None):
         print("No match for: "+ location)
         cursor.close()
