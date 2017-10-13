dc0aa32c89c96c0dfcce702a45a64184d6d2ad18
Andrew Groot <groot.andrew@gmail.com>
2017-10-13 17:46:08 -0400

    Started work on example page.  Form is present, need to call API.

---
+++ api/www/js/example.js
@@ -0,0 +1,9 @@
+$(function () {
+    // prevent standard form submission
+    $('#search-form').submit(function (event) {
+        event.preventDefault();
+
+        // TODO: perform search
+        alert("Search is not currently implemented.");
+    });
+});
