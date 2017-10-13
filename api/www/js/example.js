2cc1ef16a1d67d31a05021ecd1c6d2e7d8361fd0
Andrew Groot <groot.andrew@gmail.com>
2017-10-13 18:24:30 -0400

    Simple API call and display of results.

--- api/www/js/example.js
+++ api/www/js/example.js
@@ -3,7 +3,26 @@ $(function () {
     $('#search-form').submit(function (event) {
         event.preventDefault();
 
-        // TODO: perform search
-        alert("Search is not currently implemented.");
+        // clear current results
+        $('#results-container').html('');
+
+        // perform search
+        const formData = $('#search-form').serialize();
+        $.post('/api/v1/parking-meters?search=1', formData, function (data) {
+            for (let i in data.parkingMeters) {
+                const parkingMeter = data.parkingMeters[i];
+                $('#results-container').append("<div class=\"result\">"
+                                                    + parkingMeter.meterId
+                                                    + "<br/>"
+                                                    + (parkingMeter.isHandicap ? "Handicapped<br/>" : "")
+                                                    + (parkingMeter.isChargingStation ? "Charging Station<br/>" : "")
+                                                    + parkingMeter.location
+                                                    + "<br/>"
+                                                    + "Max Time: " + parkingMeter.maxDwellDuration + " min"
+                                                    + "<br/>"
+                                                    + "Rate (Full): $" + (parkingMeter.rateTimes100 / 100).toFixed(2)
+                                                + "</div>");
+            }
+        });
     });
 });
